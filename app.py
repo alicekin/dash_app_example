f722a1ecd2fdd9a686b99b78681f3db31d90a1ce
pandabcn <33653070+alicekin@users.noreply.github.com>
2017-12-15 04:30:34 +0800

    final version of cc project

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[3]:
 
 
 import dash
@@ -14,7 +14,7 @@ import pandas as pd
 app = dash.Dash(__name__)
 server = app.server
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
-df = pd.read_csv("C:/Users/Alice/OneDrive/cloud_computing/homework/nama_10_gdp_1_Data.csv")
+df = pd.read_csv("nama_10_gdp_1_Data.csv")
 
 available_indicators = df['NA_ITEM'].unique()
 Geo =df['GEO'].unique()
@@ -66,30 +66,19 @@ app.layout=html.Div([
     html.Div([
         html.Div([
             dcc.Dropdown(
-                id='yaxis-column_1',
+                id='yaxis-column1',
                 options=[{'label':i,'value':i} for i in available_indicators],
                 value='Imports of services'
             ),
-            dcc.RadioItems(
-                id='yaxis-type_1',
-                options=[{'label':i,'value':i} for i in ['Linear','Log']],
-                value='Linear',
-                labelStyle={'display':'inline-block'}
-            )
+          
         ],
         style={'width':'48%','float':'right','display':'inline-block'}),
         html.Div([
             dcc.Dropdown(
-                id='xaxis-column_1',
+                id='xaxis-column1',
                 options=[{'label':i,'value':i} for i in Geo],
                 value='Belgium'
             ),
-            dcc.RadioItems(
-                id='xaxis-type_1',
-                options=[{'label':i,'value':i} for i in ['Linear','Log']],
-                value='Linear',
-                labelStyle={'display':'inline-block'}
-            )
             
         ],
         style={'width':'48%', 'display':'inline-block'})
@@ -140,18 +129,20 @@ def update_graph(xaxis_column_name, yaxis_column_name,
             hovermode='closest'
         )
     }
+
+
 @app.callback(
     dash.dependencies.Output('GDP','figure'),
-    [dash.dependencies.Input('yaxis-column_1', 'value'),
-     dash.dependencies.Input('xaxis-column_1', 'value')])
+    [dash.dependencies.Input('xaxis-column1', 'value'),
+     dash.dependencies.Input('yaxis-column1', 'value')])
 
-def update_graph_1(xaxis_column_name_1, yaxis_column_name_1):
+def update_graph1(xaxis_column1_name, yaxis_column1_name):
    
-    dff = df[df['TIME'] == xaxis_column_name_1]
+    dff = df[df['GEO'] == xaxis_column1_name]
     return {
         'data': [go.Scatter(
-            x=df[dff['GEO'] == xaxis_column_name_1]['Value'],
-            y=df[dff['NA_ITEM'] == yaxis_column_name_1]['Value'],
+            x=dff[dff['NA_ITEM'] == yaxis_column1_name]['TIME'],
+            y=dff[dff['NA_ITEM'] == yaxis_column1_name]['Value'],
            
             mode='line',
             marker={
@@ -162,10 +153,10 @@ def update_graph_1(xaxis_column_name_1, yaxis_column_name_1):
         )],
         'layout': go.Layout(
             xaxis={
-                'title': xaxis_column_name_1
+                'title': xaxis_column1_name
             },
             yaxis={
-                'title': yaxis_column_name_1,
+                'title': yaxis_column1_name,
             },
             margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
             hovermode='closest'
